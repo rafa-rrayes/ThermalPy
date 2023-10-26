9956c619bea9fd27d2ca99872cee1fbb8f0b447a
rafa-rrayes <rafa@rayes.com.br>
2023-10-26 09:45:57 -0300

    tipos automatico

--- thermo.py
+++ thermo.py
@@ -4,7 +4,10 @@ import pygame
 import random
 
 global TIPOS
-TIPOS = {'ar': {'condutividade': 0.022,
+TIPOS = {'play': {'gradiente': [[201, 236, 254],[255, 153, 72]]},
+        'clear': {'gradiente': [[255,255, 0],[165, 0, 85]]},
+        'temp': {'gradiente': [[255, 95, 115], [255, 0, 0]]},
+        'ar': {'condutividade': 0.022,
                 'calor_especifico': 1.005,
                 'convectividade': 0.26,
                 'densidade': 1.225,
@@ -21,7 +24,13 @@ TIPOS = {'ar': {'condutividade': 0.022,
                 'convectividade': 0,
                 'densidade': 7860,
                 'tempMax': 1500,
-                'gradiente': [[80, 95, 115], [255, 0, 0]]}}
+                'gradiente': [[136, 140, 140], [255, 0, 0]]},
+        'pedra': {'condutividade': 1.8,
+                'calor_especifico': 1000,
+                'convectividade': 0,
+                'densidade': 2500,
+                'tempMax': 2000,
+                'gradiente': [[90, 100, 100], [255, 0, 0]]}}
 
 # Funcoes
 def gradiente(cell):
@@ -35,7 +44,7 @@ def gradiente(cell):
     cor = [(1 - multiplicador) * start[i] + multiplicador * finnish[i] for i in range(3)]
     return cor
 def generateGrid(size):
-    coords = [[Cell((i, j), 0, 'ar', 0.001) for i in range(size)] for j in range(size)]
+    coords = [[Cell((i, j), 0, 'ar', 0.01) for i in range(size)] for j in range(size)]
     for row in coords:
         for cell in row:
             cell.coords = coords
@@ -110,87 +119,69 @@ pygame.display.set_caption("Thermo Dynamics")
 botaoPlay = pygame.Rect(0, size*cell_size - 50, 100, 50)
 textoPlay = pygame.font.SysFont("Arial", 20).render("Play", True, (0, 0, 0))
 textoStop = pygame.font.SysFont("Arial", 20).render("Stop", True, (0, 0, 0))
-botaoClear = pygame.Rect(100, size*cell_size -50, 100, 50)
-textoClear = pygame.font.SysFont("Arial", 20).render("Clear", True, (0, 0, 0))
-botaoFerro = pygame.Rect(200, size*cell_size -50, 100, 50)
-textoFerro = pygame.font.SysFont("Arial", 20).render("Ferro", True, (0, 0, 0))
-botaoAgua = pygame.Rect(300, size*cell_size -50, 100, 50)
-textoAgua = pygame.font.SysFont("Arial", 20).render("Agua", True, (0, 0, 0))
-botaoAr= pygame.Rect(400, size*cell_size -50, 100, 50)
-textoAr = pygame.font.SysFont("Arial", 20).render("Ar", True, (0, 0, 0))
-botaoTemp = pygame.Rect(500, size*cell_size -50, 100, 50)
-textoTemp = pygame.font.SysFont("Arial", 20).render("Temp", True, (0, 0, 0))
 textoTemperatura = pygame.font.SysFont("Arial", 20)
+botoes = []
+posx = 0
+for tipo, info in TIPOS.items():
+        botao = pygame.Rect(posx, size*cell_size -50, 100, 50)
+        texto = pygame.font.SysFont("Arial", 20).render(tipo, True, (0, 0, 0))
+        botoes.append([botao, info, texto, tipo])
+        posx += 100
 brush_type = 'ferro'
 Jogando = True
 play = False
-
 clock = pygame.time.Clock()
-if __name__ == '__main__':
-    while Jogando:
-        if play:
-            pygame.draw.rect(screen, (255, 0, 0), botaoPlay)
-            screen.blit(textoStop, (botaoPlay.x+10, botaoPlay.y+10))
-        else:
-            pygame.draw.rect(screen, (0, 255, 0), botaoPlay)
-            screen.blit(textoPlay, (botaoPlay.x+10, botaoPlay.y+10))
-        pygame.draw.rect(screen, (80, 95, 115), botaoFerro)
-        pygame.draw.rect(screen, (0, 0, 255), botaoAgua)
-        pygame.draw.rect(screen, (255, 255, 0), botaoClear)
-        pygame.draw.rect(screen, (255, 255, 255), botaoTemp)
-        pygame.draw.rect(screen, (201, 236, 254), botaoAr)
-        screen.blit(textoClear, (botaoClear.x+10, botaoClear.y+10))
-        screen.blit(textoFerro, (botaoFerro.x+10, botaoFerro.y+10))
-        screen.blit(textoAgua, (botaoAgua.x+10, botaoAgua.y+10))
-        screen.blit(textoTemp, (botaoTemp.x+10, botaoTemp.y+10))
-        screen.blit(textoAr, (botaoAr.x+10, botaoAr.y+10))
-        pos = pygame.mouse.get_pos()
-        temp = coords[pos[0]//cell_size][pos[1]//cell_size].temperatura
-        screen.blit(        textoTemperatura.render(f'Temperatura:{temp}', True, (0, 0, 255)), (0, 0))
-        pygame.display.update()
-        if pygame.mouse.get_pressed()[0]:
-            if pos[1] < size*cell_size - 50 and brush_type != 'analise':
-                for i in range(-4,4):
-                    for j in range(-4,4):
-                        if brush_type == 'temp':
-                            coords[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura += 10
-                            coords2[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura += 10
-                        elif brush_type == 'tempMenos':
-                            coords[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura -= 10
-                            coords2[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura -= 10
-                            if coords[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura < 0:
-                                coords[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura = 0
-                                coords2[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura = 0
-                        else:
-                            coords[pos[0]//cell_size + i][pos[1]//cell_size + j].tipo = TIPOS[brush_type]
-                            coords2[pos[0]//cell_size + i][pos[1]//cell_size + j].tipo = TIPOS[brush_type]
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                Jogando = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    coords = atualizar(coords, coords2)
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                if botaoPlay.collidepoint(pos):
-                    play = not play
-                elif botaoClear.collidepoint(pos):
-                    coords = generateGrid(size)
-                    coords2 = generateGrid(size)
-                elif botaoFerro.collidepoint(pos):
-                    brush_type = 'ferro'
-                    print(calcularEnergia(coords))
-                elif botaoAgua.collidepoint(pos):
-                    brush_type = 'agua'
-                elif botaoTemp.collidepoint(pos):
+while Jogando:
+    if play: # Se a simulação estiver rodando, botão de play vira stop e vermelho
+        pygame.draw.rect(screen, (255, 0, 0), botaoPlay) 
+        screen.blit(textoStop, (botaoPlay.x+10, botaoPlay.y+10))
+    else: # Se a simulação estiver parada, botão de play vira play e verde
+        pygame.draw.rect(screen, (0, 255, 0), botaoPlay)
+        screen.blit(textoPlay, (botaoPlay.x+10, botaoPlay.y+10))
+    # Desenhar Botoes
+    for botao in botoes:
+        if botao[3] != 'play':
+            pygame.draw.rect(screen, botao[1]['gradiente'][0], botao[0])
+            screen.blit(botao[2], (botao[0].x+10, botao[0].y+10))
+
+    pos = pygame.mouse.get_pos()
+    temp = coords[pos[0]//cell_size][pos[1]//cell_size].temperatura 
+    screen.blit(textoTemperatura.render(f'Temperatura:{round(temp, 2)}', True, (0, 0, 255)), (0, 0)) # Mostrar temperatura do mouse
+    pygame.display.update()
+    if pygame.mouse.get_pressed()[0]:
+        if pos[1] < size*cell_size - 50:
+            for i in range(-4,4):
+                for j in range(-4,4):
                     if brush_type == 'temp':
-                        brush_type = 'tempMenos'
-                    else: 
-                        brush_type = 'temp'
-                elif botaoAr.collidepoint(pos):
-                    brush_type = 'ar'
-        if play:
-            pass
-            coords = atualizar(coords, coords2)
-        draw_maze(coords)
-        clock.tick(60)
+                        coords[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura += 10
+                        coords2[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura += 10
+                    elif brush_type == 'tempMenos':
+                        coords[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura -= 10
+                        coords2[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura -= 10
+                        if coords[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura < 0:
+                            coords[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura = 0
+                            coords2[pos[0]//cell_size + i][pos[1]//cell_size + j].temperatura = 0
+                    else:
+                        coords[pos[0]//cell_size + i][pos[1]//cell_size + j].tipo = TIPOS[brush_type]
+                        coords2[pos[0]//cell_size + i][pos[1]//cell_size + j].tipo = TIPOS[brush_type]
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Jogando = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                coords = atualizar(coords, coords2)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            for botao in botoes:
+                if botao[0].collidepoint(pos):
+                    if botao[3] == 'clear':
+                        coords = generateGrid(size)
+                        coords2 = generateGrid(size)
+                    elif botao[3] == 'play':
+                        play = not play
+                    else:
+                        brush_type = botao[3]
+    if play:
+        coords = atualizar(coords, coords2)
+    draw_maze(coords)
+    clock.tick(60)
